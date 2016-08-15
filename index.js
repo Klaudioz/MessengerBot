b4dafd039045b5b17f7b5a6d3b7766d33763baa0
Klaudioz <klaudioz@gmail.com>
2016-08-14 23:52:31 -0500

    Its working. Initial logic

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const bot = new BootBot({
 });
 
 bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
-    console.log('The user said "hello", "hi", "hey", or "hey there"');
+    // Send a text message followed by another text message that contains a typing indicator
+    chat.say('Hello, human friend!').then(() => {
+        chat.say('How are you today?', { typing: true });
+    });
 });
 
 bot.start(port || 5000);
