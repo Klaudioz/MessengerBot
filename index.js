52ba16f11adb26c1c4856c0fce70cf0102688286
Klaudioz <klaudioz@gmail.com>
2016-08-15 00:37:32 -0500

    Testing welcome text 0.3

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bot = new BootBot({
   appSecret: Config.FB_APP_SECRET
 });
 
-bot.setGreetingText(`Hello, ${user.first_name} !. Thanks for use our service`);
+bot.setGreetingText(`Hello. Thanks for use our service`);
 
 bot.on('attachment', (payload, chat) => {
     // Reply to the user
