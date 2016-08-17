503baa5e78b78a6230ee928da46c3bfeb9e26437
Klaudioz <klaudioz@gmail.com>
2016-08-16 22:57:31 -0500

    Starting logic - first question 0.23

--- strings.js
+++ strings.js
@@ -6,8 +6,12 @@ var words = {
     "es": "Hola"
   },
   "welcome": {
-    "en": "Welcome to BabyProgress !!, I'll be sending to you information about the progress of your baby",
+    "en": "Welcome to BabyProgress !!, I'll be sending to you information about the progress of your baby.",
     "es": "Bienvenido a BabyProgress !!, te estaré enviando información sobre el progreso de tu bebe."
+  },
+  "initial_question": {
+    "en": "Please, give me the due date or the first day of last menstrual period.",
+    "es": "Por favor dime la fecha de estimada del nacimiento de tu bebe o la de la ultima menstruación."
   },
   "months": {
     "en": ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november','december'],
