5fc563484a30a5836d5e98b9b231269aab5d37cc
Klaudioz <klaudioz@gmail.com>
2016-08-16 23:11:55 -0500

    Starting logic - adding buttons

--- strings.js
+++ strings.js
@@ -6,12 +6,16 @@ var words = {
     "es": "Hola"
   },
   "welcome": {
-    "en": "Welcome to BabyProgress !!, I'll be sending to you information about the progress of your baby.",
-    "es": "Bienvenido a BabyProgress !!, te estaré enviando información sobre el progreso de tu bebe."
+    "en": "Welcome to BabyProgress !!, I'll be sending to you information about the progress of your baby.\nBut before I've to ask you something:",
+    "es": "Bienvenido a BabyProgress !!, te estaré enviando información sobre el progreso de tu bebe.\nPero antes debo hacerte una pregunta:"
   },
-  "initial_question": {
-    "en": "Please, give me the due date or the first day of last menstrual period.",
-    "es": "Por favor dime la fecha de estimada del nacimiento de tu bebe o la de la ultima menstruación."
+  "first_question": {
+    "en": "Please, tell me the due date or the first day of last menstrual period.",
+    "es": "Por favor dime la fecha estimada del nacimiento de tu bebe o la de la ultima menstruación."
+  },
+  "first_question_btn": {
+    "en": ['Due date', 'Day last menstrual period'],
+    "es": ['Fecha nacimiento', 'Última menstruación']
   },
   "months": {
     "en": ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november','december'],
