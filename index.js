37c76ccc623f0b84438129d501d8503de214e61b
Klaudioz <klaudioz@gmail.com>
2016-08-22 17:41:46 -0500

    Asking due date personalised msg 0.2

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ bot.setGetStartedButton((payload, chat) => {
             chat.conversation((convo) => {
                 convo.ask(`${sayy(`${language}`, Strings.words.asking_due_day)}`, (payload, convo) => {
                     const text = payload.message.text;
-                    convo.set('dueDate', text);
+                    //convo.set('dueDate', text);
                     var dueDateFormatted = chrono.parseDate(text);
                     var diff = Math.floor(40 - Date.diff(dueDateFormatted, chrono.parseDate('Today')).weeks());
                     convo.say(`${sayy(`${language}`, Strings.words.your_week)} ${diff}`);
