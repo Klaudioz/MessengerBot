87b80e65e44ca255fff84a0c787efcaf7b779ee2
Klaudioz <klaudioz@gmail.com>
2016-08-22 21:04:09 -0500

    Testing message 0.61

--- strings.js
+++ strings.js
@@ -26,8 +26,8 @@ var words = {
     "es": "Por favor ingresa la fecha de tu ultima menstruación."
   },
   "your_week": {
-    "en": "You're in week: ",
-    "es": "Vas en la semana: "
+    "en": "You're in week ",
+    "es": "Vas en la semana "
   },
   "months": {
     "en": ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'],
@@ -35,7 +35,7 @@ var words = {
   },
   "weeks": {
     "en": ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22','23',
-      '24',
+      "Your baby is growing steadily, having gained about 4 ounces since last week. That puts him at about 1 1/3 pounds. Since he's almost a foot long (about the length of an ear of corn), he cuts a pretty lean figure at this point, but his body is filling out proportionally and he'll soon start to plump up. His brain is also growing quickly now, and his taste buds are continuing to develop. His lungs are developing 'branches' of the respiratory 'tree' as well as cells that produce surfactant, a substance that will help his air sacs inflate once he hits the outside world. His skin is still thin and translucent, but that will start to change soon.",
       "With her sense of movement well developed by now, your baby can feel you dance. And now that she's more than 11 inches long and weighs just over a pound (about the size of a large mango), you may be able to see her squirm underneath your clothes. Blood vessels in her lungs are developing to prepare for breathing, and the sounds that your baby's increasingly keen ears pick up are preparing her for entry into the outside world. Loud noises that become familiar now – such as your dog barking or the roar of the vacuum cleaner – probably won't faze her when she hears them outside the womb.",
        '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
     "es": []
