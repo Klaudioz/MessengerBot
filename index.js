1aeff53a2ca125144d28d8804410b104f43717ca
Klaudioz <klaudioz@gmail.com>
2016-08-22 19:40:03 -0500

    Testing message 0.4

--- index.js
+++ index.js
@@ -53,7 +53,11 @@ bot.setGetStartedButton((payload, chat) => {
                     var diff = 40 - Date.diff(dueDateFormatted, chrono.parseDate('Today')).weeks();
                     console.log(diff);
                     diff = Math.ceil(diff);
-                    convo.say(`${sayy(`${language}`, Strings.words.your_week)} ${diff}`);
+                    convo.say(`${sayy(`${language}`, Strings.words.your_week)} ${diff}`).then(() => chat.say({
+                        attachment: 'image',
+                        url: 'http://assets.babycenter.com/ims/2015/01/pregnancy-week-23-hearing_square.jpg?width=475',
+                        typing: true
+                    }));
                 });
             });
         });
