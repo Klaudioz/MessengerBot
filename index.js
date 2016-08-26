7f64dbdc1d4ca4e46ca97986b8628ee55888aa28
Klaudioz <klaudioz@gmail.com>
2016-08-25 21:53:19 -0500

    lenght function 0.25

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ bot.hear(['hi'], (payload, chat) => {
 
 bot.hear(['length'], (payload, chat) => {
     chat.getUserProfile().then((user) => {
-        chat.say(`Length in week ${weeksNum} is: ${sayy(`${language}`, Strings.words.size)[weeksNum]}`);
+        chat.say(`Length in week ${weeksNum} is: ${sayy(`${language}`, Strings.words.size)}`);
         //chat.say('I am sorry. I cant receive any attachment yet');
     });
 });
